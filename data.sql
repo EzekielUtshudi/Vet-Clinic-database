240ef700620c06f16264bb19df28a6980f1269c7
ezekiel utshudi <ezekielutshudi@gmail.com>
2022-06-02 12:46:13 +0200

    add data for multiple tables

--- data.sql
+++ data.sql
@@ -5,3 +5,50 @@ INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) V
 INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (3, 'Pikachu', '2021-01-07', 1, 'no', 15.04 );
 INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (4, 'Devimon', '2017-05-12', 5, 'yes', 11.0 );
 
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg,species) VALUES (5, 'Charmander', '2020-02-08', 0, 'no', 11.0, '' );
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg,species) VALUES (6, 'Plantmon', '2021-11-15', 2, 'yes', 5.7, '' );
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg,species) VALUES (7, 'Squirtle', '1993-04-02', 3, 'no', 12.13, '' );
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg,species) VALUES (8, 'Angemon', '2005-06-12', 1, 'yes', 45.0, '' );
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg,species) VALUES (9, 'Boarmon', '2005-06-07', 7, 'yes', 20.4, '' );
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg,species) VALUES (10, 'Blossom', '1998-10-13', 3, 'yes', 17.0, '' );
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg,species) VALUES (11, 'Ditto', '2022-05-14', 4, 'yes', 22.0, '' );
+
+/* Insert data into the owners table */
+INSERT INTO owners(full_name, age) VALUES('Sam Smith', 34);
+INSERT INTO owners(full_name, age) VALUES('Jennifer Orwell', 19);
+INSERT INTO owners(full_name, age) VALUES('Bob', 45);
+INSERT INTO owners(full_name, age) VALUES('Melody Pond', 77);
+INSERT INTO owners(full_name, age) VALUES('Dean Winchester', 14);
+INSERT INTO owners(full_name, age) VALUES('Jodie Whittaker', 38);
+
+/* Insert data into the species table */
+INSERT INTO species(name) Values('Pokemon');
+INSERT INTO species(name) Values('Digimon');
+
+/* Begin transaction */
+BEGIN;
+
+/* If the name ends in "mon" it will be Digimon */
+UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
+
+/* All other animals are Pokemon */
+UPDATE animals SET species_id = 1 WHERE name NOT LIKE '%mon';
+
+COMMIT;
+
+/* Begin transaction */
+BEGIN;
+
+/* Modify your inserted animals to include owner information (owner_id)*/
+UPDATE animals SET owner_id = 1 WHERE name LIKE 'Agumon';
+UPDATE animals SET owner_id = 2 WHERE name LIKE 'Gabumon';
+UPDATE animals SET owner_id = 2 WHERE name LIKE 'Pikachu';
+UPDATE animals SET owner_id = 3 WHERE name LIKE 'Devimon';
+UPDATE animals SET owner_id = 3 WHERE name LIKE 'Plantmon';
+UPDATE animals SET owner_id = 4 WHERE name LIKE 'Charmander';
+UPDATE animals SET owner_id = 4 WHERE name LIKE 'Squirtle';
+UPDATE animals SET owner_id = 4 WHERE name LIKE 'Blossom';
+UPDATE animals SET owner_id = 5 WHERE name LIKE 'Angemon';
+UPDATE animals SET owner_id = 5 WHERE name LIKE 'Boarman';
+
+COMMIT;
